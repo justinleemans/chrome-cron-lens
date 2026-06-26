1b01b2bad3a119bf454db905b0b1d0cd5cfd1c7f
Justin Leemans <justintentje64@gmail.com>
2026-06-26 18:55:57 +0200

    Placed new 24 hour time setting under foldout menu

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -2,13 +2,16 @@
 	<div class="flex flex-col gap-4 items-stretch mb-[1.5em]">
 		<Toggle leftLabel="Passive" rightLabel="Active" v-model="activeMode" @update:model-value="toggleActiveMode"/>
 		<Button v-if="!activeMode" :action="replaceCron">Replace</Button>
-		<Toggle leftLabel="12h" rightLabel="24h" v-model="militaryTime" @update:model-value="toggleMilitaryTime"/>
+		<Foldout closedLabel="more settings" openLabel="less settings">
+			<Toggle leftLabel="12h" rightLabel="24h" v-model="militaryTime" @update:model-value="toggleMilitaryTime"/>
+		</Foldout>
 	</div>
 	<Footer/>
 </template>
 
 <script setup>
 	import Button from "../components/input/Button.vue";
+	import Foldout from "../components/foldout/Foldout.vue";
 	import Footer from "../components/footer/Footer.vue";
 	import Toggle from "../components/input/Toggle.vue";
 
