b23d5fa804f0377e6d254433b63201e534bec0e8
Justin Leemans <justintentje64@gmail.com>
2026-06-26 18:46:12 +0200

    Added foldout to hide future settings under

---
+++ src/components/foldout/Foldout.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="open = !open" type="button" class="flex items-center justify-center cursor-pointer">
+        <span v-if="!open" class="mr-1">{{ closedLabel }}</span>
+        <ChevronDownIcon v-if="!open" class="size-3" />
+        <span v-if="open" class="mr-1">{{ openLabel }}</span>
+        <ChevronUpIcon v-if="open" class="size-3" />
+    </button>
+    <div v-if="open">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+    import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/vue/24/solid";
+    
+    import { ref } from "vue";
+    
+    const props = defineProps({
+        closedLabel: { type: String },
+        openLabel: { type: String }
+    });
+
+    const open = ref(false);
+</script>
